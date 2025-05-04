d0bee770a7a811ee66ffdc74b7324b6e547835fa
Daniil <45711483+DanHerasymenko@users.noreply.github.com>
2025-05-04 19:56:29 +0300

    new: create tokens

--- services/auth/internal/config/config.go
+++ services/auth/internal/config/config.go
@@ -3,17 +3,23 @@ package config
 import (
 	"fmt"
 	"github.com/caarlos0/env/v11"
+	"time"
 )
 
 type Config struct {
-	AuthEnv               string `env:"AUTH_APP_ENV"   envDefault:"local"`
-	AuthHostPort          string `env:"AUTH_HOST_PORT" envDefault:":8080"`
+	AuthEnv      string `env:"AUTH_APP_ENV"   envDefault:"local"`
+	AuthHostPort string `env:"AUTH_HOST_PORT" envDefault:":8080"`
+
 	PostgresContainerHost string `env:"POSTGRES_CONTAINER_HOST"`
 	PostgresContainerPort int    `env:"POSTGRES_CONTAINER_PORT"`
 	AuthPostgresUser      string `env:"AUTH_POSTGRES_USER"`
 	AuthPostgresPassword  string `env:"AUTH_POSTGRES_PASSWORD"`
 	AuthPostgresDB        string `env:"AUTH_POSTGRES_DB"`
 	RunMigrations         bool   `env:"RUN_MIGRATIONS" envDefault:"false"`
+
+	TokenSecret         string        `env:"TOKEN_SECRET"`
+	AccessTokenTTLMin   time.Duration `env:"ACCESS_TOKEN_TTL_MIN"`
+	RefreshTokenTTLDays time.Duration `env:"REFRESH_TOKEN_TTL_DAYS"`
 }
 
 func NewConfigFromEnv() (*Config, error) {
---
+++ services/auth/internal/services/auth/token.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const (
+	mins = time.Minute
+	day  = 24 * time.Hour
+)
+
+func (s *Service) CreateAccessAuthToken(userID string) (*string, error) {
+
+	accessToken, err := generateToken(userID, s.cfg.TokenSecret, s.cfg.AccessTokenTTLMin*mins)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate access token: %w", err) // ✅
+	}
+
+	return accessToken, nil
+}
+
+func (s *Service) CreateRefreshAuthToken(userID string) (*string, error) {
+
+	refreshToken, err := generateToken(userID, s.cfg.TokenSecret, s.cfg.RefreshTokenTTLDays*day)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate refresh token: %w", err)
+	}
+
+	//SAVE TOKEN TO REDIS
+
+	return refreshToken, nil
+
+}
+
+func generateToken(userID string, secret string, ttl time.Duration) (*string, error) {
+	now := time.Now()
+	claims := jwt.RegisteredClaims{
+		Subject:   userID,
+		IssuedAt:  jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(ttl)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign token: %w", err)
+	}
+
+	return &signedToken, nil
+}
